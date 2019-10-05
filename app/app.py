114a877fed4d450bfdf32bace6a2c8c8d7b836e2
Suraj <2015suraj.pawar@ves.ac.in>
2019-10-05 02:21:55 -0400

    Update: Minor

--- app/app.py
+++ app/app.py
@@ -12,35 +12,34 @@ posts = [
         'State': 'CA'
     }
 ]
-
+def compar(state, crime):
+    with open('combine.json', 'r') as f:
+        data = json.load(f)
+        lst = data["final"]
+        for each_state in lst:
+            if list(each_state.keys())[0] == state:
+                all_id = list(each_state.values())[0]
+                for eachid in all_id:
+                    if eachid[0] == crime:
+                        sp = eachid[1]
+                        return sp
+        
+    return None
 
 @app.route("/")
 def index():
     return render_template('index.html', posts=posts)
 
 @app.route("/", methods=['POST', 'GET'])
+
+    
 def fetch():
     crime = request.form['crime']
     state = request.form['state']
+    ans = compar(state, crime)
     
-    def compar(state, crime):
-        with open('combine.json', 'r') as f:
-            data = json.load(f)
-            lst = data["final"]
-            for each_state in lst:
-                if list(each_state.keys())[0] == state:
-                    d = {}
-                    v = []
-                    #d[list(lstn[ind].keys())[0]]
-                    all_id = list(each_state.values())[0]
-                    for eachid in all_id:
-                        if eachid[0] == idi:
-                            sp = eachid[1]
-                            return sp
-        
-    return None
     
-    return tuple([crime, state])
+    return ans
     # print(crime, state)
 
 
