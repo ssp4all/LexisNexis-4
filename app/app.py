78913d754ece5e733cf2a8c08141993b4477f7d8
Suraj <2015suraj.pawar@ves.ac.in>
2019-10-05 01:58:48 -0400

    Add: data extraction

--- app/app.py
+++ app/app.py
@@ -23,6 +23,23 @@ def fetch():
     crime = request.form['crime']
     state = request.form['state']
     
+    def compar(state, crime):
+        with open('combine.json', 'r') as f:
+            data = json.load(f)
+            lst = data["final"]
+            for each_state in lst:
+                if list(each_state.keys())[0] == state:
+                    d = {}
+                    v = []
+                    #d[list(lstn[ind].keys())[0]]
+                    all_id = list(each_state.values())[0]
+                    for eachid in all_id:
+                        if eachid[0] == idi:
+                            sp = eachid[1]
+                            return sp
+        
+    return None
+    
     return tuple([crime, state])
     # print(crime, state)
 
